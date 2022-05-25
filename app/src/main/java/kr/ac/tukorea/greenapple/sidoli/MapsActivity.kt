c8e9ff960b807efb07d3c014ab408bc7867c3b9e
sooohi <68193613+sooohi@users.noreply.github.com>
2022-05-25 15:55:36 +0900

    APIData.db

--- app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
+++ app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
@@ -49,6 +49,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         adb.openDatabase()
     }
 
+    //
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
