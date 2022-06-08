c0a4ec4c7dbdd6092bcc103def68cd4780172794
David Lee <tidavid@naver.com>
2022-06-08 17:07:57 +0900

    change XML

--- app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
+++ app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
@@ -43,6 +43,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         //insertLampData()
 
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
