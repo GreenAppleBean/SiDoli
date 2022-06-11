99ea18e9a451d2e89a6d66a905e3e8cf276c97e7
hi-june <wnsgur735@gmail.com>
2022-06-11 22:44:51 +0900

    semi-final update

--- app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
+++ app/src/main/java/kr/ac/tukorea/greenapple/sidoli/MapsActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.sqlite.SQLiteDatabase
 import android.location.Location
+import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.MenuItem
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
@@ -28,6 +31,13 @@ import kr.ac.tukorea.greenapple.sidoli.api_police.getPoliceAPI
 import kr.ac.tukorea.greenapple.sidoli.databinding.ActivityMapsBinding
 import kr.ac.tukorea.greenapple.sidoli.sql.AssetDatabaseOpenHelper
 
+/**
+ * 자잘한 버그들..
+ * 1. 보안등 버튼이 한 번 클릭 되면 재클릭 시 지워져도 확대, 축소 시 다시 뜸 (setOnCameraListener 때문인 것 같은데 자세한 건 알아봐야 함)
+ * 2. 안심벨 울리기는 on/off 방식으로 구현해놓긴 했는데, 실제로 울리는 지는 실제 디바이스에서 테스트해봐야할 듯!!
+ * 3. 경찰서 목록이 뜨긴 하는데, 두 번째로 클릭해야만 그에 맞는 적합한 경로가 뜸 (뭔가 클릭이 한 번씩 밀리는 것 같음)
+ */
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
@@ -36,89 +46,178 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var pp:SQLiteDatabase
     private lateinit var clusterManager: ClusterManager<LampItemData>
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient // 위칫값 사용
-    private lateinit var locationCallback: LocationCallback // 위칫값 요청에 대한 갱신 정보를 받아옴
+    private lateinit var fusedLocationClient: FusedLocationProviderClient // 위치값 사용
+    private lateinit var locationCallback: LocationCallback // 위치값 요청에 대한 갱신 정보를 받아옴
 
-    lateinit var initial_marker : Marker
-    lateinit var initial_location : Location
+    lateinit var initial_marker : Marker    // 현재 위치 마커(갱신용)
+    lateinit var initial_location : Location    // 현재 위치를 Location 객체로 저장해둠(갱신용)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        // 권한 array 저장
+        // 지도 권한을 array에 저장
         val permission = arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION)
-        requirePermissions(permission, 999)
+        requirePermissions(permission, 999) // 권한 요청을 함
 
-        // 공공데이터로부터 바로 읽어오기(경찰 데이터 부분)
-        adb = AssetDatabaseOpenHelper(this) // assets에 있는 db파일 불러오기
+        // 공공데이터로부터 바로 읽어오기()
+        adb = AssetDatabaseOpenHelper(this) // assets에 있는 db파일(램프 데이터) 불러오기
         pp = adb.openDatabase() // database 열기
 
         // 쿼리문을 통해 데이터가 있는 지 검사하기
-        insertPoliceData()
-        //insertLampData()
+        insertPoliceData()  // 경찰서 위치에 대한 데이터를 db에 추가함
+        //insertLampData()  // 보안등 관련 api 문제로 구현은 안 해놓음 (필요시 추후 업데이트 예정)
 
-        updateLocation()
-
-        // 램프 켜기 버튼(클러스터가 확대 축소 시에 무조건 다시 뜨는 버그 있음)
-        var flag1 = 0
-        Floating1.setOnClickListener{
-            if(flag1 == 0) {
-                addClusterOnMap()
-                flag1 = 1
+        // 0. 현제 위치 마커로 찍고 카메라 위치 옮기기
+        var GPS_flag = 0    // 버튼의 on/off 구현을 위한 flag 설정
+        GPS.setOnClickListener{
+            if(GPS_flag == 0) {
+                setLastLocation(initial_location)   // 현 위치로 카메라 설정 및 마커 찍기
+                GPS_flag = 1
             }
-            else if(flag1 == 1){
-                mMap.clear()
-                flag1 = 0
+            else if(GPS_flag == 1){
+                initial_marker.remove() // 현 위치 마커 지우기
+                GPS_flag = 0
             }
         }
 
-        // 현제 위치 마커로 찍고 카메라 위치 옮기기
-        var flag2 = 0
-        Floating2.setOnClickListener{
-            if(flag2 == 0) {
-                updateLocation()
-                flag2 = 1
+        // 1. 램프 켜기 버튼(클러스터가 확대 축소 시에 무조건 다시 뜨는 버그 있음)
+        var Light_flag = 0  // 버튼의 on/off 구현을 위한 flag 설정
+        LightBtn.setOnClickListener{
+            if(Light_flag == 0) {
+                addClusterOnMap()   // map에 가로등 클러스터 구현
+                Light_flag = 1
             }
-            else if(flag2 == 1){
-                initial_marker.remove()
-                //mMap.clear()
-                flag2 = 0
+            else if(Light_flag == 1){
+                mMap.clear()    // 지도에 있는 모든 표시들 지움(클러스터 일시적으로 지워지긴 하나 확대 및 축소 시 다시 살아남)
+                Light_flag = 0
             }
         }
 
-        // 경찰서 전화걸기
-        bellbtn.setOnClickListener {
+        // 2. 안심벨 울리기 버튼
+        val uriRingtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM) // 안심벨용 기본 알림음(별도 파일x, 내장용)
+        val ringtone = RingtoneManager.getRingtone(this, uriRingtone)
+        var Bell_flag = 0   // 버튼의 on/off 구현을 위한 flag 설정
+        BellBtn.setOnClickListener{
+            if(Bell_flag == 0){
+                ringtone.play() // 소리 울림
+                Bell_flag = 1
+            }
+            else if(Bell_flag == 1){
+                ringtone.stop() // 소리 끔
+                Bell_flag = 0
+            }
+        }
+
+        // 3. 경찰서 전화걸기
+        CallBtn.setOnClickListener {
             var intent = Intent(Intent.ACTION_DIAL)
-            intent.data = Uri.parse("tel:112")
+            intent.data = Uri.parse("tel:112")  // intent를 이용해 경찰서로 전화거는 기능 구현
             if(intent.resolveActivity(packageManager) != null){
                 startActivity(intent)
             }
         }
 
-        // 경찰서 위치 마커 표시
-        var police_flag = 0
-        nearbtn.setOnClickListener {
-            if(police_flag == 0){
-                addMarkerOnMap()
-                police_flag = 1
-            }
-            else if(police_flag == 1){
-                mMap.clear()
-                police_flag = 0
-            }
+        // 4. 경찰서 목록이 뜨고 그 중에서 하나 선택하면 현위치로부터 경로 찍어주기
+        PoliceBtn.setOnClickListener {
+            var popupMenu = PopupMenu(applicationContext, PoliceBtn)    // 팝업 메뉴를 통한 구현
+            menuInflater?.inflate(R.menu.menu_police, popupMenu.menu)
+
+            var listener = PopupMenuListener()  // 리스너는 아래에 내부 클래스로 구현해둠
+            popupMenu.setOnMenuItemClickListener(listener)
+            popupMenu.show()
         }
 
     }
 
-    fun startProcess(){
+    // 팝업 메뉴를 위한 내부 클래스
+    inner class PopupMenuListener:PopupMenu.OnMenuItemClickListener{
+        val policeArray = adb.PoliceDataExtract(pp) // 경찰서 위치 데이터 뽑아내기
+        override fun onMenuItemClick(p0: MenuItem?): Boolean {
+            when(p0?.itemId){   // 각각 경찰서 위치에 맞는 경로를 설정
+                R.id.world -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[0].latitude},${policeArray[0].longitude}")
+                }
+                R.id.world2 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[1].latitude},${policeArray[1].longitude}")
+                }
+                R.id.world3 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[2].latitude},${policeArray[2].longitude}")
+                }
+                R.id.world4 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[3].latitude},${policeArray[3].longitude}")
+                }
+                R.id.world5 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[4].latitude},${policeArray[4].longitude}")
+                }
+                R.id.world6 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[5].latitude},${policeArray[5].longitude}")
+                }
+                R.id.world7 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[6].latitude},${policeArray[6].longitude}")
+                }
+                R.id.world8 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[7].latitude},${policeArray[7].longitude}")
+                }
+                R.id.world9 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[8].latitude},${policeArray[8].longitude}")
+                }
+                R.id.world10 -> {
+                    mMap.clear()
+                    getcurrentDirection("${initial_location.latitude},${initial_location.longitude}",
+                        "${policeArray[9].latitude},${policeArray[9].longitude}")
+                }
+            }
+            return false
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    @SuppressLint("PotentialBehaviorOverride")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        pp = adb.openDatabase()
+
+        clusterManager = ClusterManager(this, mMap)
+        mMap.setOnCameraIdleListener(clusterManager)    // 줌 인 줌 아웃을 trigger 삼아 작동하는 듯
+        mMap.setOnMarkerClickListener(clusterManager)   // 마커 클릭 listener
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this) // 현재 위치 설정을 위한 코드
+
+        // 일단 한국공학대학교 위치를 기준으로 기본 카메라 줌 위치를 설정했습니다.
+        // 한국공학대학교(37.3401906, 126.7335293)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.3401906,126.7335293), 15f))
+
+        updateLocation()    // 현재 위치를 설정해줌
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    fun startProcess(){ // 권한이 생길 시 map을 띄워주는 메소드
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    // 지도 위치 권한 관련 메소드1
     fun requirePermissions(permissions: Array<String>, requestCode: Int) {
         if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             permissionGranted(requestCode)
@@ -132,6 +231,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    // 지도 위치 권한 관련 메소드2
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -147,7 +247,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     // 권한이 있는 경우 실행
     fun permissionGranted(requestCode: Int) {
-        startProcess() // 권한이 있는 경우 구글 지도를준비하는 코드 실행
+        startProcess() // 권한이 있는 경우 구글 지도를 준비하는 코드 실행
     }
 
     // 권한이 없는 경우 실행
@@ -158,52 +258,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .show()
     }
 
-
-    @SuppressLint("PotentialBehaviorOverride")
-    override fun onMapReady(googleMap: GoogleMap) {
-        val policeArray = adb.PoliceDataExtract(pp)
-        // 경찰 data 마커 표시하기 (파랑색)
-        for (idx: Int in 0 until policeArray.size) {
-            //val police = LatLng(policeArray[idx].latitude, policeArray[idx].longitude)
-            //mMap.addMarker(MarkerOptions().position(police).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
-            Log.d("Police", "${policeArray[idx].name}, ${policeArray[idx].latitude}, ${policeArray[idx].longitude}")
-        }
-
-        mMap = googleMap
-        pp = adb.openDatabase()
-
-        clusterManager = ClusterManager(this, mMap)
-        mMap.setOnCameraIdleListener(clusterManager)
-        mMap.setOnMarkerClickListener(clusterManager)
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-
-        // 일단 한국공학대학교 위치를 기준으로 기본 카메라 줌 위치를 설정했습니다. 추후에 gps 위치 기반으로 줌 설정하는 것도 구현해봅시다!
-        // 한국공학대학교(37.3401906, 126.7335293)
-        // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.3401906,126.7335293), 14f))
-
-        // 이부분이 길찾기 클릭 리스너에 들어가면 됩니다.
-        // getcurrentDirection("37.3401906,126.7335293", "37.3302817253012,126.68978386536874")
-        getcurrentDirection("${initial_location.latitude},${initial_location.longitude}", "${policeArray[0].latitude}, ${policeArray[0].longitude}")
-    }
-
+    ///////////////////////////////////////////////////////////////////////////////////////////////
     // 위치 정보를 받아오는 역할
     @SuppressLint("MissingPermission") //requestLocationUpdates는 권한 처리가 필요한데 현재 코드에서는 확인 할 수 없음. 따라서 해당 코드를 체크하지 않아도 됨.
-    fun updateLocation() {
-        //var LATLNG = LatLng(0.0, 0.0)
+    fun updateLocation() {  // onMapReady가 실행되고 나서 실행되어야 작동함!!
         val locationRequest = LocationRequest.create()
         locationRequest.run {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            //interval = 1000
         }
 
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
                 p0?.let {
                     for(location in it.locations) {
-                        Log.d("Location", "${location.latitude} , ${location.longitude}")
-                        setLastLocation(location)
+                        initial_location = location // 현위치 변수 초기화
+                        Log.d("Location", "${location.latitude} , ${location.longitude}")   // 확인용
                     }
                 }
             }
@@ -211,6 +280,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper()!!)
     }
 
+    // 현위치에 마커 추가하고 카메라의 위치를 옮기는 메소드
     fun setLastLocation(lastLocation: Location){
         val LATLNG = LatLng(lastLocation.latitude, lastLocation.longitude)
         val markerOptions = MarkerOptions()
@@ -221,14 +291,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .target(LATLNG)
             .zoom(15.0f)
             .build()
-        //mMap.clear()
-        initial_marker = mMap.addMarker(markerOptions)
-        initial_location = lastLocation//LatLng(lastLocation.latitude, lastLocation.longitude)
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+        initial_marker = mMap.addMarker(markerOptions)  // 마커 추가하고
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))  // 카메라 위치 옮김
     }
 
-    //////////////////////////////////////////////////////////////////////////////
-    private fun getcurrentDirection(startLoc:String, endLoc:String) {
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    // 경로 설정용 메소드
+    private fun getcurrentDirection(startLoc:String, endLoc:String) {   // 출발점 좌표와 도착점 좌표 읽어옴
         getDirectionsAPI(pp).getDirectionsData(startLoc,endLoc)
         val cursor = pp.rawQuery("SELECT * From Directions;", null)
         while (cursor.moveToNext()) {
@@ -260,6 +330,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         policeExistTest.close()   // 데이터베이스 닫기
     }
 
+    // 경찰서 위치 지도에 마커로 표시해주는 메소드(구현은 해놓았으니 실 사용은 안 하게 됨)
     private fun addMarkerOnMap() {
         val policeArray = adb.PoliceDataExtract(pp)
         // 경찰 data 마커 표시하기 (파랑색)
@@ -269,6 +340,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    // 안전등 위치 지도에 클러스터로 표시해주는 메소드
     private fun addClusterOnMap(){
         val lampArray = adb.LampDataExtract(pp)
         // for loop을 돌려서 지도에 클러스터링 마커를 찍어줌
